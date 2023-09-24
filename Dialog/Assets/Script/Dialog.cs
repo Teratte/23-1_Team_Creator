4d90c3b0e9094e6796855c124c6148b022a7588f
JaeHun Jin <ospf0306@gmail.com>
2023-09-24 23:46:57 +0900

    레거시 수정, 마우스 입력 시 다음 대사 실행 추가 중
    문장 출력에는 성공했지만, 캐릭터 이름 및 이미지를 id로 받아오는데 어떻게 대입을 해줘야 될지 모르겠음

--- Dialog/Assets/Script/Dialog.cs
+++ Dialog/Assets/Script/Dialog.cs
@@ -3,10 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialog : MonoBehaviour
 {
     private int currentDialogID = -1;
+
+    bool check_mouse = false;
+
+    public Text chat;
+    public Text char_name;
 
     private void DrawDialog()
     {
@@ -20,15 +26,45 @@ public class Dialog : MonoBehaviour
         Debug.Log("ID : "+Data.id);
         Debug.Log("캐릭터 : "+Data.characterid); //캐릭터 csv id
         Debug.Log("문장 : "+Data.sentence); // 문장
+
+
         for (int i = 0; i < Data.branch.Length; i++)
         {
-            Debug.Log("다음문장ID : "+Data.branch[i].next_sentence_id);
-            Debug.Log("대답 : "+Data.branch[i].answer);
+            Debug.Log("다음문장ID : " + Data.branch[i].next_sentence_id);
+            Debug.Log("대답 : " + Data.branch[i].answer);
         }
-        //TODO: 여기서 UI를 그려준다.
 
+        /* if (check_mouse == true) // 마우스 좌 클릭시 다음문장 실행
+         {
+             for (int i = 0; i < Data.branch.Length; i++)
+             {
+                 Debug.Log("다음문장ID : " + Data.branch[i].next_sentence_id);
+                 Debug.Log("대답 : " + Data.branch[i].answer);
+             }
+         }*/
+        //TODO: 여기서 UI를 그려준다
 
+        chat.text = Data.sentence; // 문장 출력용 텍스트 레거시
+        //char_name.text = Data.characterid;
     }
+
+    public void Start()
+    {
+        StartDialog(1);
+    }
+
+   /*public void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 마우스 좌 클릭시
+        {
+            check_mouse = true;
+        }
+        else if (Input.GetMouseButtonUp(0)) // 좌 클릭업
+        {
+            check_mouse = false;
+        }
+    }*/
+
     public void StartDialog(int SentenceId)
     {
         currentDialogID = SentenceId; //TODO: 네이밍 통일하면 좋을듯.
